03d1ac4464afd1b46cf536901273d423c7a918c1
Choseongyul <tjdduf0504@naver.com>
2024-12-30 16:07:21 +0900

    Add : assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import symbolLogo from './assets/images/symbolLogo.svg'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,24 +8,13 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={symbolLogo} className="logo" alt="매일, mail" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={symbolLogo} className="logo react" alt="매일, mail" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>타이핑은 최소로, 완성은 TAB으로.</h1>
     </>
   )
 }
