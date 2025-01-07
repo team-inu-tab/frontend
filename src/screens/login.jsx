0a295f3e5112e981e44d00430929f4f88be1e4ca
Choseongyul <tjdduf0504@naver.com>
2025-01-07 13:59:05 +0900

    feat: Add import path

---
+++ src/screens/login.jsx
@@ -0,0 +1,12 @@
+import '@css/App.css'
+import symbolLogo from '@assets/images/symbolLogo.svg'
+
+function Login() {
+  return (
+    <>
+      <img src={symbolLogo} className="logo" alt="매일, mail" />
+    </>
+  )
+}
+
+export default Login
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, './src'),
+      "@screens": path.resolve(__dirname, './src/screens'),
+      "@components": path.resolve(__dirname, './src/components'),
+      "@css": path.resolve(__dirname, './src/css'),
+      "@assets": path.resolve(__dirname, './src/assets'),
+      "@utils": path.resolve(__dirname, './src/utils')
+    },
+  },
+});
