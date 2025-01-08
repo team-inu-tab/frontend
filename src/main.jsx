e7033ab5cd5c3dcdcc621873c7e46e134046725c
Choseongyul <tjdduf0504@naver.com>
2025-01-08 14:44:47 +0900

    fix: main.jsx modify import login.jsx file name

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Login from "@/screens/Login.jsx";
+import Login from "@/screens/login.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
