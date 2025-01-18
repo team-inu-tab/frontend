8686b4e91f7b925fecb09bfa4612ad09e394994e
kyeongeunx <gyeongeh@gmail.com>
2025-01-18 23:04:41 +0900

    feat: Add mailItem component

---
+++ src/components/mailBox/mailItem.jsx
@@ -0,0 +1,35 @@
+import "@components/mailBox/css/mailItem.css";
+
+const MailItem = ({
+  hasAttachment,
+  sender,
+  subject,
+  timestamp,
+  isChecked,
+  onCheck,
+}) => {
+  return (
+    <div className="mailItem-wrapper">
+      <input
+        className="mailItem-checkBox"
+        type="checkbox"
+        checked={isChecked}
+        onChange={(e) => onCheck(e.target.checked)}
+      />
+      <div className="mailItem-mailInfo">
+        <span className="mailItem-sender">{sender}</span>
+        {hasAttachment && (
+          <img
+            className="mailItem-attachment"
+            src="/src/assets/icons/attachment.svg"
+            alt="Attachment icon for email"
+          />
+        )}
+        <span className="mailItem-subject">{subject}</span>
+        <span className="mailItem-timestamp">{timestamp}</span>
+      </div>
+    </div>
+  );
+};
+
+export default MailItem;
