837105a364c186716c206fde95e49ac3a3a52fde
kyeongeunx <gyeongeh@gmail.com>
2025-01-18 23:37:52 +0900

    feat: Add mailSenderGroup component

---
+++ src/components/mailBox/mailSenderGroup.jsx
@@ -0,0 +1,36 @@
+import "@components/mailBox/css/mailSenderGroup.css";
+import MailItem from "./mailItem";
+
+const MailSenderGroup = ({ sender, mailItems }) => {
+  return (
+    <div className="mailSenderGroup-wrapper">
+      <div className="mailSenderGroup-senderContainer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="6"
+          height="6"
+          viewBox="0 0 6 6"
+          fill="none"
+        >
+          <circle cx="3" cy="3" r="3" fill="#A87CF6" />
+        </svg>
+        <span className="mailSenderGroup-sender">{sender}</span>
+      </div>
+      <div className="mailSenderGroup-Container">
+        {mailItems.map((mail) => (
+          <MailItem
+            key={mail.id}
+            hasAttachment={mail.hasAttachment}
+            sender={mail.sender}
+            subject={mail.subject}
+            timestamp={mail.timestamp}
+            isChecked={mail.isChecked}
+            onCheck={mail.onCheck}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MailSenderGroup;
