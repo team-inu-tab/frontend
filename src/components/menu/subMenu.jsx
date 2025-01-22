bbe6d98bd1fd36f64852b048f5eb66d81210ab1c
kyeongeunx <gyeongeh@gmail.com>
2025-01-23 00:35:47 +0900

    docs:Add comments to menu components

--- src/components/menu/subMenu.jsx
+++ src/components/menu/subMenu.jsx
@@ -2,16 +2,36 @@ import SubMenuItem from "./SubMenuItem";
 import { useState } from "react";
 import "../menu/css/subMenu.css";
 
+/**
+ * @param {boolean} props.isMenubarOpen - 메뉴바가 열려 있는지 여부
+ * @returns {JSX.Element} 서브 메뉴 컴포넌트
+ */
 const SubMenu = ({ isMenubarOpen }) => {
   const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
 
+  // 메일함
+  const mailBox = [
+    "receiveMail",
+    "sentMail",
+    "selfSentMail",
+    "importantMail",
+    "scheduledMail",
+    "draftMail",
+    "spamMail",
+    "deletedMail",
+  ];
+
+  // 서브 메뉴 상태 토글 함수
   const toggleSubMenu = () => setIsSubMenuOpen((prev) => !prev);
 
   return (
     <div className={`subMenu-wrapper ${isMenubarOpen ? "" : "subMenu-close"}`}>
+      {/* 마우스 hover 시 나타나는 왼쪽 바 */}
       <div
         className={`subMenu-leftBar ${isMenubarOpen ? "" : "subMenu-close"}`}
       />
+
+      {/* 서브 메뉴 제목과 아이콘 */}
       <div
         className={`subMenu-container ${isMenubarOpen ? "" : "subMenu-close"}`}
         onClick={toggleSubMenu}
@@ -31,27 +51,30 @@ const SubMenu = ({ isMenubarOpen }) => {
             메일함
           </span>
         </div>
+
+        {/* 서브 메뉴 열림/닫힘 화살표 */}
         <img
           className={`subMenu-arrow ${isMenubarOpen ? "" : "subMenu-close"} ${
             isSubMenuOpen ? "subMenu-subMenuOpen" : ""
           }`}
           src="/src/assets/icons/arrow.svg"
+          alt="화살표 아이콘"
         />
       </div>
+
+      {/* 서브 메뉴 항목*/}
       <div
         className={`subMenu-subMenuItemsContainer ${
           isMenubarOpen ? "" : "subMenu-close"
         } ${isSubMenuOpen ? "subMenu-subMenuOpen" : ""}`}
       >
-        {["보낸 메일함", "내게 쓴 메일함", "받은 메일함", "예약 메일함"].map(
-          (title) => (
-            <SubMenuItem
-              key={title}
-              title={title}
-              isMenubarOpen={isMenubarOpen}
-            />
-          )
-        )}
+        {mailBox.map((title) => (
+          <SubMenuItem
+            key={title}
+            title={title}
+            isMenubarOpen={isMenubarOpen}
+          />
+        ))}
       </div>
     </div>
   );
