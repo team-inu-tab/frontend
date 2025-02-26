eb99f10713d5236b9b7844b9db3fae4c3fa1b548
Choseongyul <tjdduf0504@naver.com>
2025-02-26 16:15:24 +0900

    fix: google login

--- src/components/common/googleLoginButton.jsx
+++ src/components/common/googleLoginButton.jsx
@@ -1,24 +1,32 @@
-import React, { props } from 'react'
+import React from 'react';
+import axios from 'axios';
 import { GoogleLogin } from '@react-oauth/google';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const GoogleLogIn = () => {
-  return (
-    <React.Fragment>
-      <GoogleOAuthProvider clientId='클라이언트 ID'>
-        <GoogleLogin
-          className={`$className`}
-          buttonText={`$buttonText`}
-          onSuccess={(credentialResponse) => {
-            console.log(credentialResponse);
-          }}
-          onError={() => {
-            console.log('Login Failed');
-          }}
-        />
-      </GoogleOAuthProvider>
-    </React.Fragment>
-  )
-}
+  const handleSuccess = (credentialResponse) => {
+    const token = credentialResponse.credential;
 
-export default GoogleLogIn
+    axios.post("https://likelionfesival.shop/oauth2/authorization/google", { token })
+      .then((response) => {
+        console.log("로그인 성공:", response.data);
+        window.location.href = "https://festivalteama.shop";
+      })
+      .catch((error) => {
+        console.error("로그인 처리 중 오류 발생: ", error);
+      });
+  };
+
+  return (
+    <GoogleOAuthProvider clientId="클라이언트 ID">
+      <GoogleLogin
+        onSuccess={handleSuccess}
+        onError={() => {
+          console.log("로그인 실패");
+        }}
+      />
+    </GoogleOAuthProvider>
+  );
+};
+
+export default GoogleLogIn;
