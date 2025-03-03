4b3a01779446757813831986486df7a1dba2c12e
kyeongeunx <gyeongeh@gmail.com>
2025-03-03 12:36:56 +0900

    feat: Add createMailButton component

---
+++ src/components/mailBox/createMailButton.jsx
@@ -0,0 +1,10 @@
+import "@components/mailBox/css/createMailButton.css";
+
+const CreateMailButton = () => {
+  return (
+    <div className="createMailButton-wrapper">
+      <span>메일쓰기</span>
+    </div>
+  );
+};
+export default CreateMailButton;
--- src/components/mailBox/mailBoxLayout.jsx
+++ src/components/mailBox/mailBoxLayout.jsx
@@ -3,23 +3,25 @@ import MailListHeader from "./mailListHeader";
 import MenuBar from "../menu/menuBar";
 import { useMenuStore } from "../../store";
 import { Outlet } from "react-router-dom";
+import CreateMailButton from "./createMailButton";
 
 const MailBoxLayout = () => {
   const isMenuBarOpen = useMenuStore((state) => state.isMenuBarOpen);
 
   return (
-    <div className="mailBoxLayout-wrapper">
-      {/* 메뉴바 */}
-      <MenuBar />
-      <div
-        className={`mailBoxLayout-container ${
-          isMenuBarOpen ? "menuBar-open" : ""
-        }`}
-      >
-        {/* 메일함 헤더 */}
-        <MailListHeader />
-        <div className="mailBoxLayout-common">
-          <Outlet />
+    <div className="mailBoxLayout">
+      <CreateMailButton />
+      <div className="mailBoxLayout-wrapper">
+        <MenuBar />
+        <div
+          className={`mailBoxLayout-container ${
+            isMenuBarOpen ? "menuBar-open" : ""
+          }`}
+        >
+          <MailListHeader />
+          <div className="mailBoxLayout-common">
+            <Outlet />
+          </div>
         </div>
       </div>
     </div>
