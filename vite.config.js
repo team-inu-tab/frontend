2cc539900fd884e1f6de319d5736710762175383
kyeongeunx <gyeongeh@gmail.com>
2025-03-14 21:09:24 +0900

    fix: vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,37 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  plugins: [react(), svgr()],
-  server: {
-    cors: {
-      origin: "maeilmail.co.kr",
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      credentials: true,
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+
+  return {
+    plugins: [react(), svgr()],
+    server: {
+      cors: {
+        origin: isProduction ? "https://maeilmail.co.kr" : true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+      },
+      hmr: isProduction ? { host: "maeilmail.co.kr", protocol: "wss" } : true,
+      host: "0.0.0.0",
+      port: 5173,
+      allowedHosts: isProduction
+        ? ["maeilmail.co.kr"]
+        : ["localhost", "127.0.0.1"],
     },
-    host: true,
-    port: 5173,
-    allowedHosts: ["maeilmail.co.kr"],
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@screens": path.resolve(__dirname, "./src/screens"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@css": path.resolve(__dirname, "./src/screens/css"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
-      "@contexts": path.resolve(__dirname, "./src/contexts"),
-      "@routes": path.resolve(__dirname, "./src/routes"),
-      "@store": path.resolve(__dirname, "./src/store"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@screens": path.resolve(__dirname, "./src/screens"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@css": path.resolve(__dirname, "./src/screens/css"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
+        "@utils": path.resolve(__dirname, "./src/utils"),
+        "@hooks": path.resolve(__dirname, "./src/hooks"),
+        "@contexts": path.resolve(__dirname, "./src/contexts"),
+        "@routes": path.resolve(__dirname, "./src/routes"),
+        "@store": path.resolve(__dirname, "./src/store"),
+      },
     },
-  },
+  };
 });
