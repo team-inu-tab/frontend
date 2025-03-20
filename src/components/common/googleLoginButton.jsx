c82dbe12821d0c5f1a2938f8a7f20d8d15061c74
kyeongeunx <gyeongeh@gmail.com>
2025-03-20 16:12:10 +0900

    fix: connect reissue API

--- src/components/common/googleLoginButton.jsx
+++ src/components/common/googleLoginButton.jsx
@@ -1,33 +1,9 @@
 import { useRef } from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import axios from "axios";
-
-axios.defaults.withCredentials = true;
 
 const GoogleLogIn = () => {
   const linkRef = useRef(null);
-
-  // 액세스 토큰 재발급 요청 함수
-  const refreshAccessToken = async () => {
-    try {
-      const response = await axios.post(
-        "https://likelionfesival.shop/oauth2/reissue",
-        {},
-        { withCredentials: true }
-      );
-
-      const accessToken = response.headers["authorization"]; // 액세스 토큰 추출
-      if (accessToken) {
-        localStorage.setItem("accessToken", accessToken); // 저장
-        console.log("새로운 액세스 토큰:", localStorage.getItem("accessToken"));
-      } else {
-        console.error("액세스 토큰을 찾을 수 없음");
-      }
-    } catch (error) {
-      console.error("액세스 토큰 갱신 실패:", error);
-    }
-  };
 
   const handleSuccess = (credentialResponse) => {
     const token = credentialResponse.credential;
@@ -39,8 +15,6 @@ const GoogleLogIn = () => {
       linkRef.current.href = redirectUrl;
       linkRef.current.click();
     }
-
-    window.addEventListener("focus", refreshAccessToken);
   };
 
   return (
--- src/screens/signin.jsx
+++ src/screens/signin.jsx
@@ -1,16 +1,46 @@
-import React, {useState} from 'react';
-import '@css/signin.css';
-import symbolLogo from '@assets/images/symbolLogo.svg';
-import Circle from '@components/signin/circle.jsx';
-import Container from '@components/signin/parentContainer.jsx';
-import CompleteButton from '@components/signin/completeButton.jsx';
-import DropDown from '@components/signin/dropDown.jsx';
-import InputLine from '@assets/images/inputLine.svg';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "@css/signin.css";
+import symbolLogo from "@assets/images/symbolLogo.svg";
+import Circle from "@components/signin/circle.jsx";
+import Container from "@components/signin/parentContainer.jsx";
+import CompleteButton from "@components/signin/completeButton.jsx";
+import DropDown from "@components/signin/dropDown.jsx";
+import InputLine from "@assets/images/inputLine.svg";
 import backGround from "@assets/images/backGround.svg";
 
 function Signin() {
   const [selectedJob, setSelectedJob] = useState("");
   const jobData = { data: ["학생", "직장인"] };
+
+  // 리프레시 API 호출 함수
+  const refreshAccessToken = async () => {
+    try {
+      const response = await axios.post(
+        "https://likelionfesival.shop/oauth2/reissue",
+        {},
+        { withCredentials: true }
+      );
+
+      const accessToken = response.headers["authorization"]?.replace(
+        "Bearer ",
+        ""
+      );
+      if (accessToken) {
+        localStorage.setItem("accessToken", accessToken);
+        console.log("새로운 액세스 토큰 저장 완료:", accessToken);
+      } else {
+        console.error("액세스 토큰을 찾을 수 없음!");
+      }
+    } catch (error) {
+      console.error("액세스 토큰 갱신 실패:", error);
+    }
+  };
+
+  // 페이지 로드 시 리프레쉬 API 호출
+  useEffect(() => {
+    refreshAccessToken();
+  }, []);
 
   const renderJobText = (value) => {
     setSelectedJob(value);
@@ -21,27 +51,38 @@ function Signin() {
 
   return (
     <Container>
-        <img src={backGround} className='backGround'/>
-        <Circle className='formContainer'>
-          <img src={symbolLogo} className='symbolLogo'/>
-          <span className='jobPlaceHolder'>직업</span>
-          <img src={InputLine} className='inputLine1'></img>
-          <DropDown props={jobData} onSelect={renderJobText} />
-          <span className="jobText">{selectedJob}</span>
-          {selectedJob && <input className='affiliationInput'></input>}
-          {selectedJob === "학교" && <span className='departmentPlaceHolder'>학과</span>}
-          {selectedJob === "회사" && <span className='departmentPlaceHolder'>부서</span>}
-          {selectedJob && <img src={InputLine} className='inputLine2'></img>}
-          {selectedJob === "학교" && <input className='departmentInput'></input>}
-          {selectedJob === "회사" && <input className='departmentInput'></input>}
-          {selectedJob === "학교" && <span className='positionPlaceHolder'>학번</span>}
-          {selectedJob === "회사" && <span className='positionPlaceHolder'>직책</span>}
-          {selectedJob === "학교" && <input className='positionInput'></input>}
-          {selectedJob === "회사" && <input className='positionInput'></input>}
-          <CompleteButton className='addInfoCompleteButton' text="입력완료"></CompleteButton>
-        </Circle>
+      <img src={backGround} className="backGround" />
+      <Circle className="formContainer">
+        <img src={symbolLogo} className="symbolLogo" />
+        <span className="jobPlaceHolder">직업</span>
+        <img src={InputLine} className="inputLine1"></img>
+        <DropDown props={jobData} onSelect={renderJobText} />
+        <span className="jobText">{selectedJob}</span>
+        {selectedJob && <input className="affiliationInput"></input>}
+        {selectedJob === "학교" && (
+          <span className="departmentPlaceHolder">학과</span>
+        )}
+        {selectedJob === "회사" && (
+          <span className="departmentPlaceHolder">부서</span>
+        )}
+        {selectedJob && <img src={InputLine} className="inputLine2"></img>}
+        {selectedJob === "학교" && <input className="departmentInput"></input>}
+        {selectedJob === "회사" && <input className="departmentInput"></input>}
+        {selectedJob === "학교" && (
+          <span className="positionPlaceHolder">학번</span>
+        )}
+        {selectedJob === "회사" && (
+          <span className="positionPlaceHolder">직책</span>
+        )}
+        {selectedJob === "학교" && <input className="positionInput"></input>}
+        {selectedJob === "회사" && <input className="positionInput"></input>}
+        <CompleteButton
+          className="addInfoCompleteButton"
+          text="입력완료"
+        ></CompleteButton>
+      </Circle>
     </Container>
-  )
+  );
 }
 
-export default Signin
+export default Signin;
