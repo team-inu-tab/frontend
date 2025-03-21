3bbafc0cbc5e8ced94392771009d526fa0a022e8
Choseongyul <tjdduf0504@naver.com>
2025-03-21 13:33:47 +0900

    fix: googleLogin Test

--- src/components/common/googleLoginButton.jsx
+++ src/components/common/googleLoginButton.jsx
@@ -2,23 +2,18 @@ import { useRef } from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const GoogleLogIn = () => {
-  const linkRef = useRef(null);
+  const navigate = useNavigate();
 
   const handleSuccess = async (credentialResponse) => {
-    const token = credentialResponse.credential; // Google ID 토큰
-    const redirectUrl = `https://likelionfesival.shop/oauth2/authorization/google?token=${encodeURIComponent(
-      token
-    )}`;
+    const token = credentialResponse.credential;
+    const redirectUrl = `https://likelionfesival.shop/oauth2/authorization/google?token=${encodeURIComponent(token)}`;
 
     try {
       await axios.get(redirectUrl, { withCredentials: true });
-
-      if (linkRef.current) {
-        linkRef.current.href = redirectUrl;
-        linkRef.current.click();
-      }
+      navigate("/test");
     } catch (error) {
       console.error("로그인 요청 실패:", error);
     }
@@ -32,7 +27,6 @@ const GoogleLogIn = () => {
           console.log("로그인 실패");
         }}
       />
-      <a ref={linkRef} style={{ display: "none" }}></a>
     </GoogleOAuthProvider>
   );
 };
