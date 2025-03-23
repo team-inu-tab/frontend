ead42a5a0f533f242d3eb9d752052a8f12c9fbf1
kyeongeunx <gyeongeh@gmail.com>
2025-03-23 22:31:06 +0900

    feat: Add useLoadMailbox.js

---
+++ src/hooks/useLoadMailbox.js
@@ -0,0 +1,78 @@
+import { useEffect } from "react";
+import { useMailApi } from "@/hooks/useMailApi";
+import { useMailStore } from "../store";
+
+export const useLoadMailbox = (type) => {
+  const setReceivedMails = useMailStore((state) => state.setReceivedMails);
+  const setSentMails = useMailStore((state) => state.setSentMails);
+  const setDraftMails = useMailStore((state) => state.setDraftMails);
+  const setScheduledMails = useMailStore((state) => state.setScheduledMails);
+  const setSelfSentMails = useMailStore((state) => state.setSelfSentMails);
+  const setImportantMails = useMailStore((state) => state.setImportantMails);
+  const setStatus = useMailStore((state) => state.selectedsetStatusMail);
+  const setError = useMailStore((state) => state.setError);
+
+  const {
+    fetchReceiveMails,
+    fetchSentMails,
+    fetchDraftMails,
+    fetchScheduledMails,
+    fetchImportantMails,
+    fetchSelfSentMails,
+  } = useMailApi();
+
+  useEffect(() => {
+    const load = async () => {
+      setStatus("loading");
+      try {
+        let data;
+
+        switch (type) {
+          case "receive":
+            data = await fetchReceiveMails();
+            setReceivedMails(data);
+            break;
+          case "sent":
+            data = await fetchSentMails();
+            setSentMails(data);
+            break;
+          case "draft":
+            data = await fetchDraftMails();
+            setDraftMails(data);
+            break;
+          case "scheduled":
+            data = await fetchScheduledMails();
+            setScheduledMails(data);
+            break;
+          case "important":
+            data = await fetchImportantMails();
+            setImportantMails(data);
+            break;
+          case "self":
+            data = await fetchSelfSentMails();
+            setSelfSentMails(data);
+            break;
+          default:
+            throw new Error("메일 타입을 찾을 수 없습니다.");
+        }
+
+        setStatus("succeeded");
+      } catch (err) {
+        setError(err.message || "메일 불러오기 실패");
+        setStatus("failed");
+      }
+    };
+
+    load();
+  }, [
+    type,
+    setReceivedMails,
+    setSentMails,
+    setDraftMails,
+    setScheduledMails,
+    setSelfSentMails,
+    setImportantMails,
+    setStatus,
+    setError,
+  ]);
+};
