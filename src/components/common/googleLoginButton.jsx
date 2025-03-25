d680aaa298cbbc64026bd55a8aaa704f8eb88afa
Choseongyul <tjdduf0504@naver.com>
2025-03-25 15:59:11 +0900

    fix: Login Issue

--- src/components/common/googleLoginButton.jsx
+++ src/components/common/googleLoginButton.jsx
@@ -20,12 +20,7 @@ const GoogleLogIn = () => {
 
   return (
     <GoogleOAuthProvider clientId="776239999845-1l9ai960n66udm13vhcgeredtlr0e76q.apps.googleusercontent.com">
-      <GoogleLogin
-        onSuccess={handleSuccess}
-        onError={() => {
-          console.log("로그인 실패");
-        }}
-      />
+      <GoogleLogin />
       <a ref={linkRef} style={{ display: "none" }}></a>
     </GoogleOAuthProvider>
   );
