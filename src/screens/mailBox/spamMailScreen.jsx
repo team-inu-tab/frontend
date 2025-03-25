29e4c8ef222f06794c2a9be2ed0aea55f3286b65
kyeongeunx <gyeongeh@gmail.com>
2025-03-25 19:24:39 +0900

    fix: Update Google login function mapping

--- src/screens/mailBox/spamMailScreen.jsx
+++ src/screens/mailBox/spamMailScreen.jsx
@@ -1,11 +1,9 @@
-import GoogleLogIn from "../../components/common/googleLoginButton";
 import TimeSortedList from "../../components/mailBox/timeSortedList";
 
 const SpamMailScreen = () => {
   return (
     <div>
       <TimeSortedList />
-      <GoogleLogIn />
     </div>
   );
 };
