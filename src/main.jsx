daa9727c99deda39bb0ad45d2cf8c18cc6bcf0ab
kyeongeunx <gyeongeh@gmail.com>
2025-03-25 12:58:49 +0900

    chore: Uncomment router

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,18 @@ import { RouterProvider } from "react-router-dom";
 import router from "@routes/router.jsx";
 import mailListItem from "@components/mailBox/mailListItem.jsx";
 import TextEditor from "@components/common/textEditor.jsx";
-import MailWriteModal from "@components/common/mailWriteModal.jsx"
+import MailWriteModal from "@components/common/mailWriteModal.jsx";
 import Signin from "@screens/signin.jsx";
 import AddInfoComp from "@screens/addInfoComp.jsx";
 import Login from "@screens/loginScreen.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <RouterProvider router={router} /> */}
+    <RouterProvider router={router} />
     {/* <MailWriteModal></MailWriteModal> */}
     {/* <TextEditor></TextEditor> */}
     {/* <Signin></Signin> */}
     {/* <AddInfoComp></AddInfoComp> */}
     <Login></Login>
-
   </StrictMode>
 );
