663838b03cf4dca0b2e76334db38870ba0e3e9f7
Choseongyul <tjdduf0504@naver.com>
2025-03-25 15:07:45 +0900

    fix: Routing Addinfo to mail

--- src/components/signin/completeButton.jsx
+++ src/components/signin/completeButton.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {props} from 'react';
 import '@components/signin/css/completeButton.css';
 
-function CompleteButton({ className, text }) {
-    return <button className={`completeButton ${className}`} >{ text }</button>;
+function CompleteButton({ className, text, onClick }) {
+    return <button className={`completeButton ${className}`} onClick={onClick}>
+        { text }
+        </button>;
 }
 
 export default CompleteButton;
