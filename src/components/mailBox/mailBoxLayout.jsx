7686a2bfcd54fc1bcfb5daac232dbefb0796fe7d
kyeongeunx <gyeongeh@gmail.com>
2025-04-03 19:58:37 +0900

    feat: Close modal on outside click

--- src/components/mailBox/mailBoxLayout.jsx
+++ src/components/mailBox/mailBoxLayout.jsx
@@ -3,7 +3,7 @@ import MailListHeader from "./mailListHeader";
 import MenuBar from "../menu/menuBar";
 import { useMailStore, useMenuStore } from "../../store";
 import { Outlet, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useWriteStore } from "../../store/useWriteStore";
 import MailWriteModal from "../common/mailWriteModal";
 
@@ -12,7 +12,10 @@ const MailBoxLayout = () => {
   const setSelectedGroup = useMailStore((state) => state.setSelectedGroup);
   const isMenuBarOpen = useMenuStore((state) => state.isMenuBarOpen);
   const isWriteModalOpen = useWriteStore((state) => state.isWriteModalOpen);
-  const location = useLocation(); // 라우팅 경로
+  const toggleWriteModal = useWriteStore((state) => state.toggleWriteModal);
+  const location = useLocation();
+
+  const modalRef = useRef(null);
 
   useEffect(() => {
     setSelectedMail(null);
@@ -28,13 +31,32 @@ const MailBoxLayout = () => {
     }
   }, [isWriteModalOpen]);
 
+  // 모달 외부 클릭 시 닫기
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        toggleWriteModal(); // 외부 클릭 시 모달 닫기
+      }
+    };
+
+    if (isWriteModalOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isWriteModalOpen, toggleWriteModal]);
+
   return (
     <div className="mailBoxLayout-wrapper">
       {/* 모달 조건부 렌더링 */}
       {isWriteModalOpen && (
         <>
           <div className="modal-overlay" />
-          <div className="modal-wrapper">
+          <div className="modal-wrapper" ref={modalRef}>
             {/* 모달 내부 내용 */}
             <MailWriteModal />
           </div>
