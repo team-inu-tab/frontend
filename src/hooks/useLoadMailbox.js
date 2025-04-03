aec00f8b3e60af03c6d645a81333b740a6356437
kyeongeunx <gyeongeh@gmail.com>
2025-04-03 16:24:04 +0900

    fix: fetch mailbox

--- src/hooks/useLoadMailbox.js
+++ src/hooks/useLoadMailbox.js
@@ -36,30 +36,30 @@ export const useLoadMailbox = (type) => {
         switch (type) {
           case "receive":
             data = await fetchReceiveMails();
-            setReceivedMails(data);
-            setGroupedReceiveMails(data);
+            setReceivedMails(data.mails);
+            setGroupedReceiveMails(data.mails);
             break;
           case "sent":
             data = await fetchSentMails();
-            setGroupedSentMails(data);
-            setSentMails(data);
+            setGroupedSentMails(data.mails);
+            setSentMails(data.mails);
             break;
           case "draft":
             data = await fetchDraftMails();
-            setDraftMails(data);
+            setDraftMails(data.mails);
             break;
           case "important":
             data = await fetchImportantMails();
-            setImportantMails(data);
+            setImportantMails(data.mails);
             break;
           case "self":
             data = await fetchSelfSentMails();
-            setSelfSentMails(data);
+            setSelfSentMails(data.mails);
             break;
           case "spam":
             data = await fetchSpamMails();
-            console.log("스팸 메일 불러옴:", data);
-            setSpamMails(data);
+            console.log("스팸 메일 불러옴:", data.mails);
+            setSpamMails(data.mails);
             break;
           default:
             throw new Error("메일 타입을 찾을 수 없습니다.");
