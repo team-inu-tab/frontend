44d4c84050862b6ee99b2111f000261e569a5a44
kyeongeunx <gyeongeh@gmail.com>
2025-04-10 13:27:11 +0900

    fix: Close modal after mail is sent

--- src/screens/sendCompleteScreen.jsx
+++ src/screens/sendCompleteScreen.jsx
@@ -1,23 +1,27 @@
-import {react, useEffect} from 'react';
-import sendComplete from '@assets/icons/sendComplete.svg';
-import '@screens/css/sendCompleteScreen.css';
+import { useEffect } from "react";
+import sendComplete from "@assets/icons/sendComplete.svg";
+import "@screens/css/sendCompleteScreen.css";
+import { useNavigate } from "react-router-dom";
+import { useWriteStore } from "../store/useWriteStore";
 
-const SendComplete = ({ onComplete }) => {
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            if (onComplete) {
-                onComplete();
-            }
-        }, 1000);
+const SendComplete = () => {
+  const navigate = useNavigate();
+  const setWriteModalOpen = useWriteStore((state) => state.setWriteModalOpen);
 
-        return () => clearTimeout(timer);
-    }, [onComplete]);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setWriteModalOpen(false);
+      navigate(-1);
+    }, 1000);
 
-    return (
-        <div className="completeBackground">
-            <img src={sendComplete} className='sendCompleteIcon' alt="전송 완료" />
-        </div>
-    );
+    return () => clearTimeout(timer);
+  }, [setWriteModalOpen, navigate]);
+
+  return (
+    <div className="completeBackground">
+      <img src={sendComplete} className="sendCompleteIcon" alt="전송 완료" />
+    </div>
+  );
 };
 
 export default SendComplete;
