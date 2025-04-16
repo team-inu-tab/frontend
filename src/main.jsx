df57ef14abbd05bd828b583496dc70fa3bbcbc22
Choseongyul <tjdduf0504@naver.com>
2025-04-16 13:22:27 +0900

    fix: signin background

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,17 @@ import { RouterProvider } from "react-router-dom";
 import router from "@routes/router.jsx";
 import { Toaster } from "react-hot-toast";
 import MailWriteModal from "@components/common/mailWriteModal.jsx";
-// import Signin from "@screens/signin.jsx";
+import Signin from "@screens/signin.jsx";
 // import Landing from "@screens/landingScreen.jsx";
 // import SendComplete from "@screens/sendCompleteScreen.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    <Toaster position="top-center" reverseOrder={false} />
+    {/* <RouterProvider router={router} />
+    <Toaster position="top-center" reverseOrder={false} /> */}
     {/* <MailWriteModal></MailWriteModal> */}
     {/* <TextEditor></TextEditor> */}
-    {/* <Signin></Signin> */}
+    <Signin></Signin>
     {/* <AddInfoComp></AddInfoComp> */}
     {/* <MailWriteModal/> */}
     {/* <Landing/> */}
